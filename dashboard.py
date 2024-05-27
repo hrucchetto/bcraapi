f47fc39ff50a54cfb4a3850a705c6baa1b29af6d
hugo.rucchetto <hugorucchetto@hotmail.com>
2024-05-27 22:23:10 +0200

    adding metrics to dashboard

--- dashboard.py
+++ dashboard.py
@@ -28,18 +28,18 @@ class Analyzer:
             'Indice para Contratos de Locacion (ICL-Ley 27.551, con dos decimales, base 30.6.20=1)': 'ICL Index'
         }
 
-        # metrics_cols = st.columns(len(metrics_dict))
+        metrics_cols = st.columns(len(metrics_dict))
 
-        # for col, metric in zip(metrics_cols, metrics_dict):
+        for col, metric in zip(metrics_cols, metrics_dict):
 
-        #     df = pd.read_sql(
-        #         queries.LAST_AVAILABLE_VALUE.format(
-        #             variable=metric
-        #             ),
-        #             self.engine
-        #         )
+            df = pd.read_sql(
+                queries.LAST_AVAILABLE_VALUE.format(
+                    variable=metric
+                    ),
+                    self.engine
+                )
 
-        #     col.metric(label=f'{metrics_dict[metric]} ({df['date'].values[0]})', value=f'{df['last_value'].values[0]} {'%' if '%' in metric else ''}')
+            col.metric(label=metrics_dict[metric] + str(df['date'].values[0]), value=str(df['last_value'].values[0]) + str('%' if '%' in metric else ''))
 
     def __full_df(self):
 
@@ -87,7 +87,7 @@ class Analyzer:
 
         st.markdown("<h2 style='color: #1b89de;'>Last value for key variables</h2>", unsafe_allow_html=True)
         
-        #self.__metrics()
+        self.__metrics()
 
         st.markdown("<h2 style='color: #1b89de;'>Variables evolution</h2>", unsafe_allow_html=True)
 
